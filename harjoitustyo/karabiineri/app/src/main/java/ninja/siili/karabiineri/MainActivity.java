53570a3d5e54d30dd2cc835186edeea79199a584
Veera Kansikas <veera.kansikas@student.tut.fi>
2018-04-20 20:19:14 +0300

    htyö: aloitus alusta, kartta lisätty

--- harjoitustyo/karabiineri/app/src/main/java/ninja/siili/karabiineri/MainActivity.java
+++ harjoitustyo/karabiineri/app/src/main/java/ninja/siili/karabiineri/MainActivity.java
@@ -8,36 +8,16 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mGotoMapButton;
-    private Button mGotoNearestButton;
-    private Button mGotoSettingsButton;
-    private Button mGotoFeedbackButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mGotoMapButton = findViewById((R.id.bt_goto_map));
-        mGotoNearestButton = findViewById(R.id.bt_goto_nearest);
-        mGotoSettingsButton = findViewById(R.id.bt_goto_settings);
-        mGotoFeedbackButton = findViewById(R.id.bt_goto_feedback);
+        Button mButton = findViewById(R.id.map_button);
 
-        mGotoMapButton.setOnClickListener( new View.OnClickListener() {
+        mButton.setOnClickListener( new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, MapActivity.class));
-            }
-        });
-
-        mGotoSettingsButton.setOnClickListener( new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
-            }
-        });
-
-        mGotoFeedbackButton.setOnClickListener( new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, FeedbackActivity.class));
             }
         });
     }
