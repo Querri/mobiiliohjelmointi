061c40b8e53651525be184164540b0d548b16eef
Veera Kansikas <veera.kansikas@student.tut.fi>
2018-04-22 11:37:18 +0300

    htyö: karttaan oma sijainti ja marker tekiilalle

--- harjoitustyo/karabiineri/app/src/main/java/ninja/siili/karabiineri/MapActivity.java
+++ harjoitustyo/karabiineri/app/src/main/java/ninja/siili/karabiineri/MapActivity.java
@@ -1,23 +1,43 @@
 package ninja.siili.karabiineri;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapActivity extends AppCompatActivity implements
+        OnMapReadyCallback,
+        GoogleMap.OnInfoWindowClickListener,
+        ActivityCompat.OnRequestPermissionsResultCallback {
+
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private boolean mPermissionDenied = false;
 
     private GoogleMap mMap;
+
+    private static final LatLng TEKIILA = new LatLng(61.449969, 23.861945);
+    private Marker tekiila;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -25,22 +45,69 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker and move the camera
+        tekiila = mMap.addMarker(new MarkerOptions()
+                .position(TEKIILA)
+                .title("TeKiilan kiipeilyseinä")
+                .snippet("Routes: 5a - 7a"));
+        tekiila.setTag("tekiila_01");
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(TEKIILA));
+
+        // Set infowindowclicklistener and enable location
+        mMap.setOnInfoWindowClickListener(this);
+        enableMyLocation();
+    }
+
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // TODO Permission was not granted, display error dialog.
+            mPermissionDenied = false;
+        }
+    }
+
+
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
+        String placeID = (String) marker.getTag();
+        // TODO pass ID to PlaceActivity
+        startActivity(new Intent(MapActivity.this, PlaceActivity.class));
     }
 }
