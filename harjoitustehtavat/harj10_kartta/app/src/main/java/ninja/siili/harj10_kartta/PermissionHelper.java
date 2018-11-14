b62e33b2e628bef54497f58e6572dce347387f27
Veera Kansikas <veera.kansikas@student.tut.fi>
2018-11-14 13:43:34 +0200

    harj10: kartta toimii

---
+++ harjoitustehtavat/harj10_kartta/app/src/main/java/ninja/siili/harj10_kartta/PermissionHelper.java
@@ -0,0 +1,52 @@
+package ninja.siili.harj10_kartta;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import static ninja.siili.harj10_kartta.MapsActivity.MY_PERMISSIONS_REQUEST_LOCATION;
+
+// A class to help check the permission to use GPS location. Code from the courses GitHub example.
+public class PermissionHelper {
+
+    public boolean askPermission(Context context){
+        Log.d("lokasofta", "askPermission()");
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            Log.d("lokasofta", " Permission is not granted");
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                Log.d("lokasofta", "Kerran kysytty, mutta ei lupaa... Nyt ei kysytä uudestaan");
+
+            } else {
+                Log.d("lokasofta", " Request the permission");
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions((Activity) context,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_READ_LOCATION is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+
+            }
+            return false;
+        } else {
+            Log.d("lokasofta", "Permission has already been granted");
+            return true;
+        }
+    }
+}
